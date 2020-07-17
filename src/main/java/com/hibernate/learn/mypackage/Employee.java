d719eff52c60ccf1eb956426e4791c03639ef85b
subhroy <subhasis.it@gmail.com>
2020-07-17 22:52:38 +0530

    1. Hibernate configuration file created 2. Hibernate and MySQL dependency added in build.gradle file

--- src/main/java/com/hibernate/learn/mypackage/Employee.java
+++ src/main/java/com/hibernate/learn/mypackage/Employee.java
@@ -6,7 +6,11 @@ public class Employee {
     private String firstName;
     private String lastName;
 
-
+    public Employee(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
     public int getId() {
         return id;
