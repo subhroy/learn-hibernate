9da23a153855543dd5d5ccca7470f4b1b90ada39
subhroy <subhasis.it@gmail.com>
2020-07-17 20:34:44 +0530

    Initial Commit: Project Structure and Create the Persistent class

---
+++ src/main/java/com/hibernate/learn/mypackage/Employee.java
@@ -0,0 +1,34 @@
+package com.hibernate.learn.mypackage;
+
+public class Employee {
+
+    private int id;
+    private String firstNam;
+    private String lastName;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstNam() {
+        return firstNam;
+    }
+
+    public void setFirstNam(String firstNam) {
+        this.firstNam = firstNam;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
